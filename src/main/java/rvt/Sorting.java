c4c89fb89bedfa1ba0add0fb7c1d4794042fb0ea
22DP1AKaha <131352892+22DP1AKaha@users.noreply.github.com>
2024-01-31 13:03:15 +0000

    sorting uzdevms 4/5

---
+++ src/main/java/rvt/Sorting.java
@@ -0,0 +1,48 @@
+package rvt;
+
+
+public class Sorting {
+    public static int smallest(int[] array){
+        int smallest = array[0];
+
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < smallest){
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfSmallest(int[] array){
+        int smallest = array[0];
+        int smallestIndex = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] < smallest){
+                smallestIndex = i;
+                smallest = array[i];
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int smallest = table[startIndex];
+        int smallestIndex = startIndex;
+
+        for (int i = startIndex; i < table.length; i++){
+            if (table[i] < smallest){
+                smallestIndex = i;
+                smallest = table[i];
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+
+}
